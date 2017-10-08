dbc2919538a5e90b858de1b6ebe5d02917aa2b74
zhuwj <767839489@qq.com>
2017-10-08 14:55:10 +0800

    修改members模型

--- app/Models/Members.php
+++ app/Models/Members.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Members extends Model
+class Members  extends Authenticatable
 {
-    use SoftDeletes;
+    use SoftDeletes, Notifiable;
 }
