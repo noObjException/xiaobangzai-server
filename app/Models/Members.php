8d0933eb165a4a719e943d1b7bf631124b01adf8
zhuwj <767839489@qq.com>
2017-10-08 14:59:14 +0800

    修改members模型

--- app/Models/Members.php
+++ app/Models/Members.php
@@ -5,8 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Members  extends Authenticatable
+class Members  extends Authenticatable implements JWTSubject
 {
     use SoftDeletes, Notifiable;
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
