8119bde0787c98b74291f354a1fc73d099d6ae6f
zhuwj <767839489@qq.com>
2017-10-18 16:06:45 +0800

    修改积分记录排序

--- app/Api/V1/Controllers/Member/CreditRecordController.php
+++ app/Api/V1/Controllers/Member/CreditRecordController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Api\V1\Controllers\Member;
 
 
@@ -11,9 +12,11 @@ class CreditRecordController extends BaseController
 {
     public function index(CreditRecords $model)
     {
-        $data = $model->where('openid', current_member_openid())->paginate(request('per_page', 15));
+        $data = $model->where('openid', current_member_openid())
+                      ->orderBy('id', 'desc')
+                      ->paginate(request('per_page', 15));
 
         return $this->response->paginator($data, new CreditRecordTransformers())
-                              ->addMeta('total_credit', current_member_info('credit'));
+            ->addMeta('total_credit', current_member_info('credit'));
     }
 }
