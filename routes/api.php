36f35395522f32404fe7ec2300ac92361634aef7
zhuwj <767839489@qq.com>
2017-11-01 20:04:03 +0800

    fix路由错误

--- routes/api.php
+++ routes/api.php
@@ -18,66 +18,69 @@
 
 $api = app('Dingo\Api\Routing\Router');
 
-// 微信支付回调通知, 不经过jwt中间件
-$api->any('/wxNotify', 'App\Api\V1\Controllers\Wechat\PaymentController@wxNotify');
 
 $api->version('v1', [
     'namespace'  => 'App\Api\V1\Controllers',
-    'middleware' => ['jwt.auth'],
 ], function ($api) {
 
+    // 微信支付回调通知, 不经过jwt中间件
+    $api->any('/wxNotify', 'Wechat\PaymentController@wxNotify');
 
-    $api->resource('/index', 'IndexController');
+
+    $api->group(['middleware' => ['jwt.auth']], function ($api) {
+        $api->resource('/index', 'IndexController');
 
 
-    // 任务(取快递, 等...)
-    $api->group(['namespace' => 'Mission'], function ($api) {
+        // 任务(取快递, 等...)
+        $api->group(['namespace' => 'Mission'], function ($api) {
 
-        $api->get('/getExpress/create', 'ExpressController@create');
-        $api->resource('/getExpress', 'ExpressController');
+            $api->get('/getExpress/create', 'ExpressController@create');
+            $api->resource('/getExpress', 'ExpressController');
 
-        // 订单流程
-        $api->get('/expressMission/index', 'OrderController@index');
-        $api->get('/expressMission/create', 'OrderController@create');
-        $api->put('/expressMission/pay/{id}', 'OrderController@pay');
-        $api->put('/expressMission/completed/{id}', 'OrderController@completed');
-        $api->put('/expressMission/addBounty/{id}', 'OrderController@addBounty');
-        $api->put('/expressMission/cancel/{id}', 'OrderController@cancel');
-        $api->put('/expressMission/acceptOrder/{id}', 'OrderController@acceptOrder');
+            // 订单流程
+            $api->get('/expressMission/index', 'OrderController@index');
+            $api->get('/expressMission/create', 'OrderController@create');
+            $api->put('/expressMission/pay/{id}', 'OrderController@pay');
+            $api->put('/expressMission/completed/{id}', 'OrderController@completed');
+            $api->put('/expressMission/addBounty/{id}', 'OrderController@addBounty');
+            $api->put('/expressMission/cancel/{id}', 'OrderController@cancel');
+            $api->put('/expressMission/acceptOrder/{id}', 'OrderController@acceptOrder');
+        });
+
+        $api->group(['namespace' => 'Member'], function ($api) {
+
+            $api->get('/schoolAreas', 'AddressController@chooseAreas');
+            $api->get('/members', 'MemberController@show');
+
+            $api->get('/memberAddress/create', 'AddressController@create');
+            $api->put('/setDefaultAddress/{id}', 'AddressController@setDefaultAddress');
+            $api->resource('/memberAddress', 'AddressController');
+
+            $api->get('/creditRecords', 'CreditRecordController@index');
+
+        });
+
+        $api->group(['namespace' => 'Common'], function ($api) {
+
+            $api->post('/pictures', 'PictureController@store');
+            $api->get('/authMember', 'AuthMemberController@authMember');
+
+        });
+
+        $api->group(['namespace' => 'Staff'], function ($api) {
+
+            $api->resource('/staffs', 'StaffController');
+            $api->get('/missions', 'MissionController@index');
+
+        });
+
+        $api->group(['namespace' => 'Wechat'], function ($api) {
+
+            $api->get('/jsSDKConfig', 'JSSDKController@getConfig');
+            $api->post('/wxPay', 'PaymentController@wxPay');
+
+
+        });
     });
 
-    $api->group(['namespace' => 'Member'], function ($api) {
-
-        $api->get('/schoolAreas', 'AddressController@chooseAreas');
-        $api->get('/members', 'MemberController@show');
-
-        $api->get('/memberAddress/create', 'AddressController@create');
-        $api->put('/setDefaultAddress/{id}', 'AddressController@setDefaultAddress');
-        $api->resource('/memberAddress', 'AddressController');
-
-        $api->get('/creditRecords', 'CreditRecordController@index');
-
-    });
-
-    $api->group(['namespace' => 'Common'], function ($api) {
-
-        $api->post('/pictures', 'PictureController@store');
-        $api->get('/authMember', 'AuthMemberController@authMember');
-
-    });
-
-    $api->group(['namespace' => 'Staff'], function ($api) {
-
-        $api->resource('/staffs', 'StaffController');
-        $api->get('/missions', 'MissionController@index');
-
-    });
-
-    $api->group(['namespace' => 'Wechat'], function ($api) {
-
-        $api->get('/jsSDKConfig', 'JSSDKController@getConfig');
-        $api->post('/wxPay', 'PaymentController@wxPay');
-
-
-    });
 });
