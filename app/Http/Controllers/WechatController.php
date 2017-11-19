886d8094f48a13115944e29f811fa06caea10e2a
zhuwj <767839489@qq.com>
2017-11-19 09:41:05 +0800

    小程序验证

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -75,7 +75,7 @@ class WechatController extends Controller
         $tmpStr = sha1( $tmpStr );
 
         if( $tmpStr == $signature ){
-            return true;
+            return 'success';
         }else{
             return false;
         }
